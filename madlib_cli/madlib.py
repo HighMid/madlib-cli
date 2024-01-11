6d7034ffc25e75ae709f1a96c8db5383cce48606
HighMid <xstar671@gmail.com>
2024-01-11 13:57:00 -0500

    Updated, reverted the test back to default

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,7 +1,9 @@
-import time, sys, re
+import time, sys, re, os
 
 
 def read_template(file):
+    
+    print("Current directory:", os.getcwd())
     
     """
     Reads the given text file, returns it as a string and checks to see if the file exists
@@ -68,102 +70,138 @@ def display_and_save_madlib(completed_madlib, output_file):
 
 ## Below is code I wish to work on to allow user inputs to affect the story
 
-# def delay_print(a):
-#     for b in a:
-#         sys.stdout.write(b)
-#         sys.stdout.flush()
-#         time.sleep(0.05)
+def delay_print(a):
+    for b in a:
+        sys.stdout.write(b)
+        sys.stdout.flush()
+        time.sleep(0.05)
 
-# def delay_print_fast(a):
-#     for b in a:
-#         sys.stdout.write(b)
-#         sys.stdout.flush()
-#         time.sleep(0.001)
+def delay_print_fast(a):
+    for b in a:
+        sys.stdout.write(b)
+        sys.stdout.flush()
+        time.sleep(0.001)
 
-# def pause(option):
-#     if option == 1:
-#         time.sleep(0.5)
+def pause(option):
+    if option == 1:
+        time.sleep(0.5)
 
-#     if option == 2:
-#         time.sleep(1)
+    if option == 2:
+        time.sleep(1)
         
-#     if option == 3:
-#         time.sleep(2)
+    if option == 3:
+        time.sleep(2)
 
-# def start():
-#     print("Skip intro?")
-#     while True:
-#         try:
-#             choice = input("Yes/No : ")
-#             if choice.lower() in ['y', 'yes']:
-#                 return True
-#             elif choice.lower() in ['n', 'no']:
-#                 return False
-#             else:
-#                 print("Ah don't worry about it, you'll like the intro guy!")
-#                 return False
-#         except ValueError:
-#             print("It's ok, I get too lost in the sauce as well, I'll just send you to the intro guy.")
-#             return True
+def start():
+    print("Skip intro?")
+    while True:
+        try:
+            choice = input("Yes/No : ")
+            if choice.lower() in ['y', 'yes']:
+                return True
+            elif choice.lower() in ['n', 'no']:
+                return False
+            else:
+                print("Ah don't worry about it, you'll like the intro guy!")
+                return False
+        except ValueError:
+            print("It's ok, I get too lost in the sauce as well, I'll just send you to the intro guy.")
+            return True
 
-# def intro(skipped):
+def intro(skipped):
     
-#     if not skipped:
-#         delay_print(". . .\n")
-#         pause(1)
-#         delay_print("It seems it's just us here.\n")
-#         pause(1)
-#         delay_print("With no one else around . . . who knows what could happen here . . .\n")
-#         pause(1)
-#         print("I hope you're understanding what I'm getting at", end=' ')
-#         delay_print(". . . =) \n")
-#         pause(3)
-#         delay_print_fast("HELL YEAH THAT'S RIGHT WE'RE GONNA PLAY MADLIB, WOOHOO\n\n")
+    if not skipped:
+        delay_print(". . .\n")
+        pause(1)
+        delay_print("It seems it's just us here.\n")
+        pause(1)
+        delay_print("With no one else around . . . who knows what could happen here . . .\n")
+        pause(1)
+        print("I hope you're understanding what I'm getting at", end=' ')
+        delay_print(". . . =) \n")
+        pause(3)
+        delay_print_fast("HELL YEAH THAT'S RIGHT WE'RE GONNA PLAY MADLIB, WOOHOO\n\n")
 
-#     if skipped:
-#         delay_print_fast(". . .\n")
-#         delay_print_fast("It seems it's just us here.\n")
-#         delay_print_fast("With no one else around . . . who knows what could happen here . . .\n")
-#         delay_print_fast("I hope you're understanding what I'm getting at . . . =) \n")
-#         delay_print_fast("HELL YEAH THAT'S RIGHT WE'RE GONNA PLAY MADLIB, WOOHOO\n")
+    if skipped:
+        delay_print_fast(". . .\n")
+        delay_print_fast("It seems it's just us here.\n")
+        delay_print_fast("With no one else around . . . who knows what could happen here . . .\n")
+        delay_print_fast("I hope you're understanding what I'm getting at . . . =) \n")
+        delay_print_fast("HELL YEAH THAT'S RIGHT WE'RE GONNA PLAY MADLIB, WOOHOO\n\n")
 
-# def rules():
+def rules():
     
-#     choice = input("Do you know the rules? Yes/No: ")
+    choice = input("Do you know the rules? Yes/No: ")
     
-#     while True:
+    while True:
         
-#         if choice.lower() in ['y', "yes"]:
+        if choice.lower() in ['y', "yes"]:
             
-#             delay_print_fast("Oh hell yeah save me the trouble!\n")
-#             return
+            delay_print_fast("Oh hell yeah save me the trouble!\n")
+            return
             
-#         if choice.lower() in ['n', 'no']: 
-#             delay_print("Yeah I got you don't worry, it's simple\n")
-#             delay_print("Now watch as I word vomit the rules!\n\n")
-#             pause(1)
-#             print("Madlibs is a fun word game where players create a story by providing words (typically nouns, verbs, adjectives, etc.)")
-#             print("to fill in blanks in a pre-written story or sentence. I will prompt you with the appropriate words when needed! ")
-#             pause(3)
-#             return
+        if choice.lower() in ['n', 'no']: 
+            delay_print("Yeah I got you don't worry, it's simple\n")
+            delay_print("Now watch as I word vomit the rules!\n\n")
+            pause(1)
+            print("Madlibs is a fun word game where players create a story by providing words (typically nouns, verbs, adjectives, etc.)")
+            print("to fill in blanks in a pre-written story or sentence. I will prompt you with the appropriate words when needed! ")
+            pause(3)
+            return
         
-#         else:
-#             print("Mhm yes, so what you're saying is that you want the rules, got it, NO PROBLEM.")
-#             print("Madlibs is a fun word game where players create a story by providing words (typically nouns, verbs, adjectives, etc.)")
-#             print("to fill in blanks in a pre-written story or sentence. I will prompt you with the appropriate words when needed! ")
-#             pause(3)
-#             return
+        else:
+            print("Mhm yes, so what you're saying is that you want the rules, got it, NO PROBLEM.")
+            print("Madlibs is a fun word game where players create a story by providing words (typically nouns, verbs, adjectives, etc.)")
+            print("to fill in blanks in a pre-written story or sentence. I will prompt you with the appropriate words when needed! ")
+            pause(3)
+            return
 
-# def main():
-#     skipped = start()
-#     intro(skipped)
-#     rules()
+def storytemplate(template):
+    
+    delay_print("Ok friend here is what we got to work with!\n\n")
+    print(template)
+    delay_print("Let's fill in those {Adjectives} and the like, with words shall we!\n\n")
 
-#     template = read_template('madlib_template.txt')
-#     stripped_template, parts = parse_template(template)
-#     user_inputs = [input(f"Enter a {part}: ") for part in parts]
-#     completed_madlib = merge(stripped_template, user_inputs)
-#     display_and_save_madlib(completed_madlib, 'output_file.txt')
+def incompletestory(template, parts):
+    
+    delay_print("Alright friend I got a story for you.\n")
+    delay_print("Let's start filling in this story with your own words.\n")
+    print("I'll keep track of what you currently here after each input, I GOT YOU.")
+    
+    user_inputs = []
+    
+    while True:
+        
+        print(template)
+        
+        for word in parts:
+            
+            print("Ok friend it looks like I'm going to need a/an ," , word , "for this one!")
+            user_input = input("What do you have for us?: ")
+            user_inputs.append(user_input)
+            delay_print("Here's how it's looking right now friend! \n")
+            current_story = merge(template, user_inputs + list(parts[len(user_inputs):]))
+            print(current_story)
+            exit()
+            
+        
 
-# if __name__ == "__main__":
+def main():
+    skipped = start()
+    intro(skipped)
+    rules()
+    
+    template = read_template('madlib_cli\madlib_template.txt')
+    
+    storytemplate(template)
+    
+    stripped_template, parts = parse_template(template)
+    
+    print(stripped_template, parts)
+    incompletestory(template, parts)
+    # user_inputs = [input(f"Enter a {part}: ") for part in parts]
+    # completed_madlib = merge(stripped_template, user_inputs)
+    # display_and_save_madlib(completed_madlib, 'output_file.txt')
+
+if __name__ == "__main__":
     main()
--- tests/test_madlib.py
+++ tests/test_madlib.py
@@ -1,24 +1,20 @@
-import pytest, os
+import pytest
 from madlib_cli.madlib import read_template, parse_template, merge
 
 
 def test_read_template_returns_stripped_string():
     actual = read_template("madlib_cli/madlib_template.txt")
-    expected = """Welcome to the Void of MadLib!
-
-I the {Adjective} and {Adjective} {A First Name} have {Past Tense Verb} {A First Name}'s {Adjective} sister and plan to steal her {Adjective} {Plural Noun}!"""
-
+    expected = "It was a {Adjective} and {Adjective} {Noun}."
     assert actual == expected
 
 
+
 def test_parse_template():
-    template = """Welcome to the Void of MadLib!
-
-I the {Adjective} and {Adjective} {A First Name} have {Past Tense Verb} {A First Name}'s {Adjective} sister and plan to steal her {Adjective} {Plural Noun}!"""
-    expected_stripped = "Welcome to the Void of MadLib!\n\nI the {} and {} {} have {} {}'s {} sister and plan to steal her {} {}!"
-    expected_parts = ("Adjective", "Adjective", "A First Name", "Past Tense Verb", "A First Name", "Adjective", "Adjective", "Plural Noun")
-
-    actual_stripped, actual_parts = parse_template(template)
+    actual_stripped, actual_parts = parse_template(
+        "It was a {Adjective} and {Adjective} {Noun}."
+    )
+    expected_stripped = "It was a {} and {} {}."
+    expected_parts = ("Adjective", "Adjective", "Noun")
 
     assert actual_stripped == expected_stripped
     assert actual_parts == expected_parts
@@ -26,14 +22,8 @@ I the {Adjective} and {Adjective} {A First Name} have {Past Tense Verb} {A First
 
 
 def test_merge():
-    stripped_template = "Welcome to the Void of MadLib!\n\nI the {} and {} {} have {} {}'s {} sister and plan to steal her {} {}!"
-    user_inputs = ["scary", "funny", "Bob", "jumped", "Alice", "beautiful", "red", "apples"]
-    expected = """Welcome to the Void of MadLib!
-
-I the scary and funny Bob have jumped Alice's beautiful sister and plan to steal her red apples!"""
-
-    actual = merge(stripped_template, user_inputs)
-    
+    actual = merge("It was a {} and {} {}.", ("dark", "stormy", "night"))
+    expected = "It was a dark and stormy night."
     assert actual == expected
 
 
